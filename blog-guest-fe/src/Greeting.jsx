b9b11f502e0d008aeeda5ea9c883cca93b91f087
FishShalami <fishshalami@gmail.com>
2025-09-14 14:10:05 -0700

    moved backend code to api directory

---
+++ blog-guest-fe/src/Greeting.jsx
@@ -0,0 +1,10 @@
+function Greeting() {
+  return (
+    <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>
+  );
+}
+
+function Goodbye() {
+  return <h2>Goodbye, for now!</h2>;
+}
+export { Greeting, Goodbye };
